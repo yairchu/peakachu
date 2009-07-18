71c8d76cd6d8cff4fa792edca94951ade7e24561
Yair Chuchem <yairchu@yair-chuchems-macbook.local>
2009-07-18 17:47:14 +0300

    chess board background

--- testFrp.hs
+++ testFrp.hs
@@ -27,9 +27,28 @@ f (x, y) =
     lighting $= Enabled
     light (Light 0) $= Enabled
     position (Light 0) $= Vertex4 0 0 (-1) 0
-    renderPrimitive Triangles $ do
-      forM_ (zip vs (tail vs ++ [head vs])) $ \((ax, ay), (bx, by)) -> do
+    chessBoard
+    drawCursor
+  where
+    chessBoard =
+      forM_ [0..7] $ \bx ->
+      forM_ [0..7] $ \by -> do
         let
+          col = 0.2 + 0.1 * (fromIntegral ((bx + by) `mod` 2))
+          r ba va = 0.125*((fromIntegral ba*2+va)-7)
+        materialDiffuse Front $= Color4 col col col 1
+        normal $ Normal3 0 0 (-1 :: GLfloat)
+        renderPrimitive Quads .
+          forM vs $ \(vx, vy) ->
+            vertex $ Vertex4 (r bx vx) (r by vy) 0 1
+    drawCursor =
+      renderPrimitive Triangles .
+      forM_ (zip vs (tail vs ++ [head vs])) $
+      \((ax, ay), (bx, by)) -> do
+        let
+          r v = (fromIntegral (round (4 * v + 0.5)) - 0.5) / 4
+          rx = r x
+          ry = r y
           points =
             [[0.9*x, 0.9*y, 0.9]
             ,[rx + 0.125*ax, ry + 0.125*ay, 1]
@@ -40,18 +59,13 @@ f (x, y) =
             | last norml < 0 = norml
             | otherwise = map negate norml
         normal $ Normal3 nx ny nz
-        materialDiffuse Front $= Color4 0 1 0 1
+        materialDiffuse Front $= Color4 1 1 0 1
         forM_ (take 1 points) $ \[px, py, pz] ->
           vertex $ Vertex4 px py 0 pz
-        materialDiffuse Front $= Color4 0 1 0 0
+        materialDiffuse Front $= Color4 1 1 0 0
         forM_ (tail points) $ \[px, py, pz] ->
           vertex $ Vertex4 px py 0 pz
-  where
     vs = [((-1), (-1)), ((-1), 1), (1, 1), (1, (-1))]
-    r v = (fromIntegral (round (4 * v + 0.5)) - 0.5) / 4
-    rx = r x
-    ry = r y
-    cs = 0.05
 
 main :: IO ()
 main = glutRun $ emap f mouseMotionEvent
