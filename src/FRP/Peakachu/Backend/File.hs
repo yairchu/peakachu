e793ccbfd4dacb2bfae4d6f4a1e8161003468dcd
Yair Chuchem <yairchu@yair-chuchems-macbook.local>
2009-07-29 17:21:43 +0300

    fixed readFileE. it was using lazy IO

--- src/FRP/Peakachu/Backend/File.hs
+++ src/FRP/Peakachu/Backend/File.hs
@@ -4,8 +4,34 @@ module FRP.Peakachu.Backend.File (
 
 import FRP.Peakachu.Internal (Event, SideEffect(..), ejoin)
 
+import Control.Monad (join)
+import Data.Function (fix)
+import System.IO (IOMode(ReadMode), openFile, hClose, hGetChar)
+import System.IO.Error (try, isEOFError)
+
+maybeIO :: (IOError -> Bool) -> IO a -> IO (Maybe a)
+maybeIO isExpected =
+  join . fmap f . try
+  where
+    f (Right x) = return $ Just x
+    f (Left err)
+      | isExpected err = return Nothing
+      | otherwise = ioError err
+
+-- Lazy IO forbidden because imho it is horrible
+strictReadFile :: FilePath -> IO String
+strictReadFile filename = do
+  file <- openFile filename ReadMode
+  contents <- fix $ \rest -> do
+    mc <- maybeIO isEOFError $ hGetChar file
+    case mc of
+      Nothing -> return ""
+      Just c -> fmap (c :) rest
+  hClose file
+  return contents
+
 readFileE :: Event FilePath -> Event String
-readFileE = ejoin . fmap readFile
+readFileE = ejoin . fmap strictReadFile
 
 writeFileE :: Event (FilePath, String) -> SideEffect
 writeFileE = SideEffect . fmap (uncurry writeFile)
