1385ff64f921a5f066cc125d41f91789dcecc0b7
Yair Chuchem <yairchu@yair-chuchems-macbook.local>
2009-11-16 23:47:01 +0200

    added filterC

--- src/Control/FilterCategory.hs
+++ src/Control/FilterCategory.hs
@@ -1,9 +1,10 @@
 module Control.FilterCategory
   ( FilterCategory(..)
-  , rid, mapMaybeC
+  , rid, mapMaybeC, filterC, getterToCond
   ) where
 
 import Control.Category (Category(..))
+import Control.Monad (guard)
 import Data.Foldable (Foldable(..), toList)
 
 import Prelude hiding ((.), id)
@@ -12,11 +13,20 @@ class Category cat => FilterCategory cat where
   flattenC :: cat [a] a
   arrC :: (a -> b) -> cat a b
 
-rid :: (FilterCategory cat, Foldable f)
-  => cat (f a) a
+rid :: (FilterCategory cat, Foldable f) => cat (f a) a
 rid = flattenC . arrC toList
 
-mapMaybeC :: (FilterCategory cat, Foldable f)
-  => (a -> f b) -> cat a b
+mapMaybeC :: FilterCategory cat => (a -> Maybe b) -> cat a b
 mapMaybeC f = rid . arrC f
+
+filterC :: FilterCategory cat => (a -> Bool) -> cat a a
+filterC cond =
+  mapMaybeC f
+  where
+    f x = do
+      guard $ cond x
+      return x
+
+getterToCond :: (a -> Maybe b) -> (a -> Bool)
+getterToCond = fmap (not . null . toList)
 
